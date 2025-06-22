517081d1ea9ab24f149466367e70a8c448d37fb6
AzizVerse <benmahmoud0499@gmail.com>
2025-06-22 23:08:44 +0100

    three

--- src/main/java/com/mybank/controller/AgencyDocumentController.java
+++ src/main/java/com/mybank/controller/AgencyDocumentController.java
@@ -93,7 +93,7 @@ public class AgencyDocumentController implements Serializable {
 
         } catch (Exception e) {
             uploadMessage = "❌ Erreur lors de l'envoi du fichier.";
-            e.printStackTrace();
+            
         }
     }
 
